ef7a10ae8edfe0a1689de8a6e4ab952e8bf2aa2b
johs <johannhk>
2017-04-21 15:14:03 +0200

    started person.h

--- temp_ws/src/trollnode-master/include/trollnode/expression_class.h
+++ temp_ws/src/trollnode-master/include/trollnode/expression_class.h
@@ -25,3 +25,28 @@ private:
 	Expression buffer[];
 };
 
+/*actionUnit* set_current_expression(const std::string & expression)
+{
+	static std::map< std::string, actionUnit * > lookup = {
+		{ "angry", angry },
+		{ "smile", smile },
+		{ "neutral", neutral},
+		{ "happy", happy },
+		{ "sad", sad },
+		{ "surprise", surprise },
+		{ "suspicious", suspicious },
+		{ "disgust", disgust },
+		{ "pain", pain },
+	};
+
+	if (expression.compare("") != 0) {
+		auto it = lookup.find(expression);
+		if (it != lookup.end()) {
+			return it->second;
+		} else {
+			ROS_ERROR("Did not recognize expression [%s], current expression not changed.",expression.c_str());
+			return NULL;
+		}
+	}
+	return NULL;
+}
